abb230c28732620878104a3bb1623aad6cdfeac6
shravankumarherekar <shravan.herekar01@gmail.com>
2023-06-13 07:25:15 +0530

    Video 12 completed

---
+++ src/module1.mjs
@@ -0,0 +1,5 @@
+import ui, { a, b } from "./module2.mjs";
+
+console.log(a);
+console.log(b);
+console.log(ui);
---
+++ src/module2.mjs
@@ -0,0 +1,7 @@
+const a = "shravan";
+const b = "kumar";
+const c = "herekar";
+
+export default c;
+export { a };
+export { b };
